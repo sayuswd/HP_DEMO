67e40d678da2e592c2376c521bbb7cbcd2b9fce0
sayuswd <sisi1013hola@yahoo.co.jp>
2019-06-27 12:57:32 +0900

    Update1
    Update1

--- Website-master/source/javascripts/base.js
+++ Website-master/source/javascripts/base.js
@@ -1,6 +1,13 @@
-javascript:
 $(function () {
-  $('.language p').click(function () {
-    $(this).next('ul').slideToggle();
+  $('.drawer').drawer({
+    iscroll: {
+      scrollX: false,
+      scrollY: true,
+      click: true,
+      mouseWheel: true,
+      disablePointer: true,
+      disableTouch: false,
+      disableMouse: false
+    },
   });
 });
